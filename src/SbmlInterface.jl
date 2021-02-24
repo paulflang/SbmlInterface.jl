8256a30589d744b7c7017e06dad362cf9be61231
paulflang <paul.lang@wolfson.ox.ac.uk>
2021-02-24 22:27:15 +0100

    try fixing ci.yml for all os (libsbml installation)

--- src/SbmlInterface.jl
+++ src/SbmlInterface.jl
@@ -1,9 +1,14 @@
 module SbmlInterface
 
 using ModelingToolkit
+export ODESystem, ODEProblem
 using OrdinaryDiffEq
+export solve, FunctionMap, Euler, Heun, Ralston, Midpoint, RK4, ExplicitRK, OwrenZen3, OwrenZen4, OwrenZen5,
+       BS3, BS5, RK46NL, DP5, Tsit5, DP8, Vern6, Vern7, Vern8, TanYam7, TsitPap8,
+       Vern9, Feagin10, Feagin12, Feagin14, CompositeAlgorithm, Anas5, RKO65, FRK65, PFRK87, RKM
 
 ENV["PYTHON"] = Sys.which("python")
+Pkg.build("PyCall")
 using PyCall
 
 const libsbml = PyNULL()
