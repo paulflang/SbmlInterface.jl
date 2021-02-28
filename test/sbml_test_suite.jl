76c692e1656bfa4b2ca15737e9c25fb6a72023fb
anand jain <33790004+anandijain@users.noreply.github.com>
2021-02-28 21:29:00 +0000

    cleanup ci.yml comments clean up sbml_test_suite.jl and @show equations output

--- test/sbml_test_suite.jl
+++ test/sbml_test_suite.jl
@@ -3,7 +3,6 @@ run(`git clone https://github.com/sbmlteam/sbml-test-suite $(test_suite_clone_pa
 path = joinpath(test_suite_clone_path, "cases")
 @test isdir(path)
 
-
 function extract_xmls(path)
     dirs = filter(isdir, readdir(path; join=true))
     all_dirs = mapreduce(x -> filter(isdir, readdir(x; join=true)), vcat, dirs)
@@ -35,20 +34,14 @@ all_files = extract_xmls(path)
 @test all_files isa Vector{String}
 
 n = length(all_files)
-skipidx = 10
-num = 1000
-# all_files = all_files[1:n]
+@test n > 0
 
-
-@time models = getmodel.(all_files); #   8.972979 seconds (779.59 k allocations: 35.538 MiB, 0.57% gc time, 0.73% compilation time)
-@time ps = getparameters.(models); # 15.963854 seconds (6.03 M allocations: 346.563 MiB, 0.86% gc time, 0.02% compilation time)
-
-@time u0s = call_save_err(getinitialconditions, models); #  16.213772 seconds (7.04 M allocations: 408.899 MiB, 0.99% gc time, 0.51% compilation time)
-@time eqs = call_save_err(getodes, models); #  52.578963 seconds (24.88 M allocations: 1.315 GiB, 1.52% gc time, 0.87% compilation time)
-@time systems = call_save_err(sbml2odesystem, all_files); #  97.208173 seconds (44.64 M allocations: 2.364 GiB, 1.50% gc time, 0.57% compilation time)
-# @time probs = call_save_err(x->sbml2odeproblem(x, (0., 1.)), all_files); # longer than im willing to wait
-@time probs = call_save_err(x->sbml2odeproblem(x, (0., 1.)), all_files[1:100]); # 18.651824 seconds (37.88 M allocations: 2.416 GiB, 3.23% gc time)
-# @time probs = call_save_err(x->ODEProblem(x...), syss[1:100]); # don't do this
+models = getmodel.(all_files); 
+ps = getparameters.(models); 
+u0s = call_save_err(getinitialconditions, models); 
+eqs = call_save_err(getodes, models); 
+systems = call_save_err(sbml2odesystem, all_files); 
+probs = call_save_err(x->sbml2odeproblem(x, (0., 1.)), all_files); 
 
 errored_mask = @. typeof(systems) <: Exception
 errs = systems[errored_mask, :]
@@ -56,6 +49,8 @@ good = systems[.!(errored_mask), :]
 
 good_probs = probs[.!(@. typeof(probs) <: Exception)] # eventually benchmark solve on these
 
-@time mtks = filtermap(modelingtoolkitize, good_probs); 
+@time mtks = filtermap(modelingtoolkitize, good_probs);
 eqs = unique(equations.(mtks))
 @test length(eqs) > 0 
+
+@show eqs 
